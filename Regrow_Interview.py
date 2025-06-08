28e7afc207e7b7da5078d818ab87ba6fc410ec11
lenawang33 <lena.wang33@gmail.com>
2025-06-08 13:34:25 -0700

    Adding specific groups to csv, simplify coding part and adding Regrow coding script

---
+++ Regrow_Interview.py
@@ -0,0 +1,17 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import seaborn as sns
+#%%
+#Reading in the Kansas data
+ks_qaqc = "https://raw.githubusercontent.com/lenawang33/Regrow_interview/main/Kansas_Duplicates.csv"
+ks_qaqc_df = pd.read_csv(ks_qaqc)
+
+#%% Completing Kansas Variance Calculation
+
+
+#%% Completing Kansas Precision Calculation
+
+#%% Completing Kansas Coeffcient of Variance Calculation
